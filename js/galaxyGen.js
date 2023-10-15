0b34da4066c8a2c2e76e4bc64b3ea3e8c22a10d5
drnoir <chrispgodber@gmail.com>
2023-10-15 23:42:18 +0100

    - CONDENSE INTO GALAXYGEN

--- js/galaxyGen.js
+++ js/galaxyGen.js
@@ -1,8 +1,9 @@
+                                            // GALAXY GEN BY CHRIS GODBER ^_^ 
+                                            // A-FRAME COMPONENT LIBRARY FOR GENEREATING GALAXY SCENES 0_0
+                                            
 // script for generating buildings / game logic etc
+// componenet library for galaxygen 
 // global game vars
-import {generatePlanet} from './generatePlanetSurface.js';
-import {genNames, genGalaxyName} from './planetGen.js';
-
 
 let starsNum = getRandomInt(20, 50);
 let gamestarted = false;
@@ -275,38 +276,7 @@ AFRAME.registerComponent('neb', {
     }
 
 });
-// planet
-// AFRAME.registerComponent('planet', {
-//     init: function () {
-//         this.direction = 1;
-//         this.position = new THREE.Vector3();
-//         this.position.copy(this.el.object3D.position);
-//         this.player = document.getElementById("rig")
-//         setTimeout(() => {
-//             this.ready = true;
-//         }, 3000);
-//     },
-//     tick: function () {
-//         if (!this.ready) return;
-//         let playerPos = this.player.object3D.position;
-//         let planetPos = this.el.object3D.position;
-//         let distance = playerPos.distanceTo(planetPos)
-//         if (distance <= 2) {
-//             console.log("planetis nearby" + this.el.id);
-//             speech.text = "Caution - you are approaching a planet";
-//             window.speechSynthesis.speak(speech);
-//         }
-//         // let position = this.el.object3D.position.y;
-//         this.el.object3D.rotation.set(
-//             THREE.Math.degToRad(15),
-//             THREE.Math.degToRad(30),
-//             THREE.Math.degToRad(90)
-//         );
-//         this.el.object3D.rotation.x += Math.PI;
-//         // this.el.object3D.position.set(this.position.x, position + 0.05 * this.direction, this.position.z);
-//     }
-//
-// });
+
 
 // cursor listener
 AFRAME.registerComponent('cursor-listener', {
@@ -684,6 +654,180 @@ function createStars(amount) {
 
     // randomPlanetId = 'planet' + [getRandomInt(0, planetnames.length)];
 }
+
+// Planet generation 
+var digrams = "ABOUSEITILETSTONLONUTHNO" +
+    "..LEXEGEZACEBISOUSESARMAINDIREA.ERATENBERALAVETIEDORQUANTEISRION";
+
+function rotatel(x) {
+    var tmp = (x & 255) * 2;
+    if (tmp > 255) tmp -= 255;
+    return tmp;
+}
+
+function twist(x) {
+    return (256 * rotatel(x / 256)) + rotatel(x & 255);
+}
+
+function next(seeds) {
+    return seeds.map(function (seed) {
+        return twist(seed);
+    });
+}
+
+function tweakseed(seeds) {
+    var tmp;
+
+    tmp = seeds.reduce(function (total, seed) {
+        return total += seed;
+    }, 0);
+
+    return seeds.map(function (seed, index, arr) {
+        return arr[index + 1] || (tmp & 65535)
+    });
+};
+
+
+function makename(pairs, seeds) {
+    var name = [];
+    /* Modify pair if you want to have names shorter or longer than 8 chars */
+    /* I'll leave that as an exercise for you. */
+    var pair = [0, 0, 0, 0];
+    var longname = seeds[0] & 64;
+
+    pair = pair.map(function () {
+        seeds = tweakseed(seeds);
+        return 2 * ((seeds[2] / 256) & 31);
+    });
+
+    pair.forEach(function (value, index, arr) {
+        if (longname || (index < (arr.length - 1))) {
+            name.push(pairs[value]);
+            name.push(pairs[value + 1]);
+        }
+    });
+
+    return name.join('').toLowerCase()
+        .replace(/^\w/, function (letter) {
+            return letter.toUpperCase();
+        });
+}
+
+function genNames(numPlanets) {
+    var names = [];
+    var pairs;
+    var num = numPlanets
+    var seeds = [23114, 584, 46931];
+    pairs = digrams.substring(24);
+
+    while (--num) {
+        names.push(makename(pairs, seeds));
+        seeds = tweakseed(next(seeds));
+    }
+
+    return names;
+}
+
+function genGalaxyName() {
+    let randGalaxyName = '';
+    let pairs;
+    let seeds = [ getRandomInt(400,60000),getRandomInt(100,700),getRandomInt(200,60000)];
+    pairs = digrams.substring(24);
+
+    randGalaxyName = makename(pairs, seeds);
+    seeds = tweakseed(next(seeds));
+
+    return randGalaxyName;
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generatePlanet(color, areaSize){
+    let planetSectorArray = [];
+    let playerCam = document.createElement('a-entity');
+    playerCam.setAttribute('camera','')
+    playerCam.setAttribute('wasd-controls', 'acceleration:100')
+    playerCam.setAttribute('look-controls','');
+    playerCam.setAttribute('id','playerCam');
+    playerCam.setAttribute('class','playerCam')
+    playerCam.setAttribute('playerCam','')
+    playerCam.setAttribute('position', {x: 0, y: 10, z: 0});
+    playerCam.setAttribute('body', {type: 'dynamic', mass: "0.5", linearDamping: "0.5"});
+    const planetScene = document.getElementById("planetScene");
+    planetScene.appendChild(playerCam);
+    let i;
+    let sectorPos = 0;
+    let prevSectorX = 0;
+    let prevSectorZ =0;
+    for (i = 0; i <   areaSize ; i++) {
+        generatePlanetSector(color, i, prevSectorX, prevSectorZ);
+        planetSectorArray.push(i)
+    }
+}
+
+function generatePlanetSector(color, id, prevSectorX, prevSectorZ){
+    const startingSectorNum = 0;
+    let newPlanetElm = document.createElement('a-entity');
+    let seedVal = 10;
+    let randomDressingArr = ['none', 'cubes', 'pyramids', 'cylinders', 'towers', 'mushrooms', 'trees', 'apparatus', 'torii'];
+    let randomDressingID = getRandomInt(0,randomDressingArr.length-1);
+    let randomDressing = randomDressingArr[randomDressingID];
+    let randomGroundArr = ['none','flat', 'hills', 'canyon', 'spikes', 'noise'];
+    let randomGroundID = getRandomInt(0,randomGroundArr.length-1);
+    let ground =  randomGroundArr[randomGroundID];
+    let dressingAmount = getRandomInt(1,15);
+    let dressingVariance = getRandomInt(1,15);
+    let yScale = getRandomInt(1,60);
+    let dressingCol = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+    // assign sector x and Y post based on prev or pos 0 if first sector
+    let randomX = id > 0 ? prevSectorX + 200 : 0; let randomZ =  id > 0 ? prevSectorZ + 200 : 0;
+    newPlanetElm.setAttribute('environment', 'preset:default;skyType:gradient;seed:25;ground:'+ground+';groundYScale:'+yScale+';dressing:'+randomDressing+';+dressingAmount:'+dressingAmount+';dressingScale:'+dressingVariance+';dressingColor:'+dressingCol+';groundTexture:walkernoise; groundColor:'+color);
+    newPlanetElm.setAttribute('active', true);
+    newPlanetElm.setAttribute('position', {x: randomX, y:1, z: 0});
+    document.getElementById('planetScene').appendChild(newPlanetElm);
+    // add mana
+    let randomMana= getRandomInt(5,100);
+    generateMana(randomMana);
+    console.log('new planet surface genearted');
+}
+
+function generateMana(numMana){
+    const planetScene = document.getElementById("planetScene");
+    let i;
+    for (i = 0; i <  numMana ; i++) {
+        let randomX = getRandomInt(1, 800);
+        let randomZ = getRandomInt(1, 800);
+        let mana = document.createElement('a-sphere');
+        let randomScale= getRandomInt(1, 3);
+        mana.object3D.scale.set(randomScale, randomScale, randomScale);
+        mana.setAttribute('name', 'mana');
+        mana.setAttribute('id', 'mana'+i);
+        mana.setAttribute('class', 'mana');
+        mana.setAttribute('material', 'src', '#energy');
+        mana.setAttribute('material', 'color', 'blue');
+        mana.setAttribute('position', {x: randomX, y:1, z: randomZ });
+        mana.setAttribute('body', {type: 'static', mass: "1"});
+        planetScene.appendChild(mana);
+    }
+}
+
+export { generatePlanet};
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomColor(colors) {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
+
 //draw the background stars
 AFRAME.registerComponent('star-system', {
     schema: {
